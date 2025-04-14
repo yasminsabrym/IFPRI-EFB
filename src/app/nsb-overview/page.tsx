f04d0c879fde505ca444dd129fd4e7c409643dc8
Yasmin Sabry <yasminsabry099@gmail.com>
2025-04-14 10:34:58 +0000

    add interactive spider chart

--- src/app/nsb-overview/page.tsx
+++ src/app/nsb-overview/page.tsx
@@ -1,9 +1,55 @@
-
 'use client';
 
 import React from 'react';
 import {Button} from '@/components/ui/button';
 import {useRouter} from 'next/navigation';
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Tooltip,
+  Legend,
+} from 'recharts';
+
+const data = [
+  {
+    subject: 'HDDS',
+    Baseline: 4.5,
+    Control: 3.86,
+    NSB: 5.37,
+    fullMark: 6,
+  },
+  {
+    subject: 'FIES',
+    Baseline: 2.5,
+    Control: 3.21,
+    NSB: 1.3,
+    fullMark: 5,
+  },
+  {
+    subject: 'Energy Intake',
+    Baseline: 1675,
+    Control: 1521,
+    NSB: 2027,
+    fullMark: 3000,
+  },
+  {
+    subject: 'Protein Intake',
+    Baseline: 45,
+    Control: 31.1,
+    NSB: 55.7,
+    fullMark: 100,
+  },
+  {
+    subject: 'Iron Intake',
+    Baseline: 8,
+    Control: 6.18,
+    NSB: 9.87,
+    fullMark: 20,
+  },
+];
 
 const NSBOverview = () => {
   const router = useRouter();
@@ -20,10 +66,34 @@ const NSBOverview = () => {
       </div>
 
       <div className="w-full max-w-xl">
-        {/* Placeholder for Spider Chart */}
-        <div className="h-80 bg-gray-200 rounded-lg mb-4">
-          <p className="text-center text-black">Interactive Spider Chart will go here</p>
-        </div>
+        {/* Interactive Spider Chart */}
+        <RadarChart cx={300} cy={250} outerRadius={150} width={600} height={500} data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" stroke="#fff" />
+          <PolarRadiusAxis angle={30} domain={[0, 'dataMax']} stroke="#fff" />
+          <Radar
+            name="Baseline"
+            dataKey="Baseline"
+            stroke="#999999"
+            fill="#999999"
+            fillOpacity={0.6}
+          />
+          <Radar
+            name="Control"
+            dataKey="Control"
+            stroke="#EE6363"
+            fill="#EE6363"
+            fillOpacity={0.6}
+          />
+          <Radar name="NSB" dataKey="NSB" stroke="#44BBA4" fill="#44BBA4" fillOpacity={0.6} />
+          <Tooltip />
+          <Legend
+            wrapperStyle={{
+              paddingTop: '20px',
+              color: '#777',
+            }}
+          />
+        </RadarChart>
         <div className="text-sm text-gray-400 text-center">
           • Baseline (Grey line) • Control Group Mid-Term (Red line) • NSB Mid-Term (Green line)
           <br />
