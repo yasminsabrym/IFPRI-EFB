67abbaba7f534e623f28154809f8f11284a19826
Yasmin Sabry <yasminsabry099@gmail.com>
2025-04-15 10:55:05 +0000

    put this in 2 months as icon

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -15,6 +15,7 @@ import {
 import {Dialog, DialogContent, DialogHeader, DialogTitle} from './dialog';
 import {motion} from 'framer-motion';
 import {useMediaQuery} from '@/hooks/use-media-query';
+import Image from 'next/image';
 
 const BaselineBaby = () => (
   <svg width="100%" height="100%" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -26,13 +27,13 @@ const BaselineBaby = () => (
 );
 
 const TwoMonthsBaby = () => (
-  <svg width="100%" height="100%" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="50" cy="50" r="45" fill="#70C16E"/>
-    <text x="50" y="50" textAnchor="middle" dominantBaseline="middle" fontSize="20" fill="white">
-      2M
-    </text>
-  </svg>
-);
+    <Image
+      src="/two-months-baby.png" // Path to your image
+      alt="Two Months Baby"
+      width={80}  // Adjust as needed
+      height={80} // Adjust as needed
+    />
+  );
 
 const FourMonthsBaby = () => (
   <svg width="100%" height="100%" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
