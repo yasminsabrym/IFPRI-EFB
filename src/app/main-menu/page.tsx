ca423108dabfe9aa57cb2c1a1963f9616ddccbfd
Yasmin Sabry <yasminsabry099@gmail.com>
2025-04-16 07:13:59 +0000

    logo does not appear

--- src/app/main-menu/page.tsx
+++ src/app/main-menu/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import {useRouter} from 'next/navigation';
 import {Button} from '@/components/ui/button';
 import {motion} from 'framer-motion'; // Import framer-motion
+import {Logo} from '@/components/logo';
 
 const MainMenu = () => {
   const router = useRouter();
@@ -33,6 +34,7 @@ const MainMenu = () => {
       animate={{opacity: 1, transition: {duration: 1}}}
       exit={{opacity: 0, transition: {duration: 0.5}}}
     >
+      <Logo/>
       <motion.h1
         className="text-4xl md:text-5xl font-semibold mb-4 text-center"
         style={{color: '#FF6301'}}
