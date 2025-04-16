c39fc5ca9890189f6cba64751db9290eb5c5d996
Yasmin Sabry <yasminsabry099@gmail.com>
2025-04-16 07:45:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: React does not recognize the `whileHover` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `whilehover` instead. If you accidentally passed it from a parent component, remove it from the DOM element.. Error source: src/components/ui/button.tsx (46:7) @ _c
    >
    >   44 |     const Comp = asChild ? Slot : "button"
    >   45 |     return (
    > > 46 |       <Comp
    >      |       ^
    >   47 |         className={cn(buttonVariants({ variant, size, className }))}
    >   48 |         ref={ref}
    >   49 |         {...props}
    >
    > Call Stack
    > 20
    >
    > Show 17 ignore-listed frame(s)
    > button
    > <anonymous> (0:0)
    > _c
    > src/components/ui/button.tsx (46:7)
    > MainMenu
    > src/app/main-menu/page.tsx (56:11)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import {motion} from "framer-motion";
 
 import { cn } from "@/lib/utils"
 
@@ -36,17 +39,21 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean,
+  whileHover?: any,
+  whileTap?: any
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+  ({ className, variant, size, asChild = false,  whileHover, whileTap, ...props }, ref) => {
+    const Comp = asChild ? Slot : motion.button
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
+        whileHover={whileHover}
+        whileTap={whileTap}
       />
     )
   }
