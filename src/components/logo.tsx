7d96c9608215c79a4e17c4a9e90db375902a1a15
Yasmin Sabry <yazzmin0011@gmail.com>
2025-04-17 12:48:07 +0200

    logo fix

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,16 +2,12 @@
 
 import Image from 'next/image';
 
-export const Logo = () => {
-  return (
-    <Image
-      src="https://drive.google.com/uc?export=view&id=1BY3mYNYBw07F5rYgNCUUlFztzvMzARu0"
-      alt="Egyptian Food Bank Logo"
-      width={150}
-      height={50}
-      style={{
-        marginBottom: '20px',
-      }}
-    />
-  );
-};
+export const Logo = () => (
+  <Image
+    src="/images/efb-logo.png"
+    alt="Egyptian Food Bank Logo"
+    width={150}
+    height={50}
+    style={{ marginBottom: '20px' }}
+  />
+);
